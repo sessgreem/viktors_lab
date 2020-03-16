09278e8198c38857f38ddf3bb924802268911b86
Viktor Ivanov <sessgreem@gmail.com>
2020-03-16 13:19:17 +0200

    order service updated - only the user can update password

--- src/app/order.service.ts
+++ src/app/order.service.ts
@@ -1,108 +1,134 @@
-import { AuthService } from './auth.service';
-import { AngularFirestore, AngularFirestoreDocument, AngularFirestoreCollection } from '@angular/fire/firestore';
-import { Injectable } from '@angular/core';
-import { Router } from '@angular/router';
-import { switchMap, map } from 'rxjs/operators';
+import { Observable, of } from "rxjs";
+import { AuthService } from "./auth.service";
+import {
+  AngularFirestore,
+  AngularFirestoreDocument,
+  AngularFirestoreCollection
+} from "@angular/fire/firestore";
+import { Injectable } from "@angular/core";
+import { Router } from "@angular/router";
+import { switchMap, map, first } from "rxjs/operators";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root"
 })
 export class OrderService {
-
-  constructor(private afs: AngularFirestore,
+  constructor(
+    private afs: AngularFirestore,
     private auth: AuthService,
-    private router: Router) {}
+    private router: Router
+  ) {}
 
-    getUserOrders() {
-      return this.auth.user$.pipe(
-        switchMap(user => {
-          return this.afs
-            .collection('orders', ref => ref.where('orderUid', '==', user.uid))
-            .snapshotChanges()
-            .pipe(
-              map(actions => {
-                return actions.map(a => {
-                  const data: Object = a.payload.doc.data();
-                  const id = a.payload.doc.id;
-                  return { id, ...data };
-                });
-              })
-            );
-        })
-      );
-    }
+  getUserOrders() {
+    return this.auth.user$.pipe(
+      switchMap(user => {
+        return this.afs
+          .collection("orders", ref => ref.where("orderUid", "==", user.uid))
+          .snapshotChanges()
+          .pipe(
+            map(actions => {
+              return actions.map(a => {
+                const data: Object = a.payload.doc.data();
+                const id = a.payload.doc.id;
+                return { id, ...data };
+              });
+            })
+          );
+      })
+    );
+  }
 
   async setOrder(object) {
-     const id = this.afs.createId();
-     const orderRef: AngularFirestoreDocument<any> = this.afs.collection('orders').doc(id);
+    const id = this.afs.createId();
+    const orderRef: AngularFirestoreDocument<any> = this.afs
+      .collection("orders")
+      .doc(id);
     const { uid } = await this.auth.getUser();
     const status = "Waiting for booster";
     const boostedRank = object.currentRank;
     const data = {
-      orderUid : uid,
-      orderStatus : status,
-      orderServiceType :object.serviceType,
-      orderCurrentRank : object.currentRank,
+      orderUid: uid,
+      orderStatus: status,
+      orderServiceType: object.serviceType,
+      orderCurrentRank: object.currentRank,
       orderCurrentDivision: object.currentDivision,
-      orderDesiredRank : object.desiredRank,
-      orderBoostedRank : boostedRank,
-      orderServer : object.server,
-      orderPriority : object.priority,
-      orderPrice : object.price,
-      orderUsername : '',
-      orderPassword : ''
-      }
-      return orderRef.set(data, {merge: true});
-    }
+      orderDesiredRank: object.desiredRank,
+      orderBoostedRank: boostedRank,
+      orderServer: object.server,
+      orderPriority: object.priority,
+      orderPrice: object.price,
+      orderUsername: "",
+      orderPassword: ""
+    };
+    return orderRef.set(data, { merge: true });
+  }
   async setOrderV2(object) {
-      const id = this.afs.createId();
-      const orderRef: AngularFirestoreDocument<any> = this.afs.collection('orders').doc(id);
-     const { uid } = await this.auth.getUser();
-     const status = "Waiting for booster";
-     const boostedRank = object.currentRank;
-     const data = {
-       orderUid : uid,
-       orderStatus : status,
-       orderServiceType :object.serviceType,
-       orderCurrentRank : object.currentRank,
-       orderCurrentDivision: object.currentDivision,
-       orderDesiredRank : object.desiredRank,
-       orderBoostedRank : boostedRank,
-       orderServer : object.server,
-       orderPriority : object.priority,
-       orderPrice : object.price,
-       orderUsername : '',
-       orderPassword : ''
-       }
-       return orderRef.set(data, {merge: true});
-     }
+    const id = this.afs.createId();
+    const orderRef: AngularFirestoreDocument<any> = this.afs
+      .collection("orders")
+      .doc(id);
+    const { uid } = await this.auth.getUser();
+    const status = "Waiting for booster";
+    const boostedRank = object.currentRank;
+    const data = {
+      orderUid: uid,
+      orderStatus: status,
+      orderServiceType: object.serviceType,
+      orderCurrentRank: object.currentRank,
+      orderCurrentDivision: object.currentDivision,
+      orderDesiredRank: object.desiredRank,
+      orderBoostedRank: boostedRank,
+      orderServer: object.server,
+      orderPriority: object.priority,
+      orderPrice: object.price,
+      orderUsername: "",
+      orderPassword: ""
+    };
+    return orderRef.set(data, { merge: true });
+  }
+
+  getOrderUid(orderId) {
+    const orderRef: AngularFirestoreDocument<any> = this.afs
+      .collection("orders")
+      .doc(orderId);
+
+    let orderObs = orderRef.valueChanges().pipe(
+      map(order => {
+        // console.log(
+        //   "in getOrderUid - " + order + "returns - " + order.orderUid
+        // );
+        return order;
+      })
+    );
+    return orderObs.pipe(first()).toPromise();
+  }
+
   async updatePassword(orderId, password) {
-      const { uid } = await this.auth.getUser();
+    const { uid } = await this.auth.getUser();
+    const { orderUid } = await this.getOrderUid(orderId);
+    // const { orderPassword } = await this.getOrderUid(orderId);
+    // console.log(
+    //   "in updatePassword " +
+    //     orderUid +
+    //     " uid  -" +
+    //     uid +
+    //     " orderPassword " +
+    //     orderPassword
+    // );
+    const data = {
+      orderPassword: password
+    };
 
-      const data = {
-        orderPassword: password
-      }
-      // i have to check if orderUid === this.uid
-      // how to get orderUid? how to get specific field from document - maybe do like getuser did
-      const orderRef: AngularFirestoreDocument<any> = this.afs.collection('orders').doc(orderId);
-      // cant seem to do be able to do anything with this reference ^
-      // let orderUidv2 = orderRef.valueChanges();
-      // console.log('orderUid is - ' + orderUidv2);
-      
-      if (uid) {
-      return orderRef.update(data).then(res => console.log('Password Update successful'));
-      }
+    const orderRef: AngularFirestoreDocument<any> = this.afs
+      .collection("orders")
+      .doc(orderId);
 
+    if (uid === orderUid) {
+      return orderRef
+        .update(data)
+        .then(res => console.log("Password Update Successful"));
+    } else {
+      console.log("Did not update password - " + orderUid);
     }
+  }
 }
-
-// orderId,
-// orderUid,
-// orderStatus,
-// orderServiceType,
-// orderCurrentRank,
-// orderDesiredRank,
-// orderBoostedRank,
-// orderServer,
-// orderPriority,
-// orderPrice
