074d1536c2ab6f0e1fe9ba70f80a1e30b22740b1
Viktor Ivanov <sessgreem@gmail.com>
2020-09-20 20:01:02 +0300

    Separted variables into another file, added mixin for gradient

--- src/app/order-menu-v2/order-menu-v2.component.ts
+++ src/app/order-menu-v2/order-menu-v2.component.ts
@@ -1,5 +1,13 @@
 import { Component, OnInit } from "@angular/core";
-import { VirtualTimeScheduler } from "rxjs";
+import {
+  leagues,
+  servers,
+  queues,
+  summoners,
+  roles,
+  leaguePoints,
+  divisions,
+} from "../shared/variables";
 
 @Component({
   selector: "app-order-menu-v2",
@@ -16,166 +24,42 @@ export class OrderMenuV2Component implements OnInit {
   primaryRoleDropdownOpened = false;
   secondaryRoleDropdownOpened = false;
 
-  // put in a separate file
-  leagues: any[] = [
-    { id: 0, name: "Iron", imgURL: "../../assets/ranks-images/Iron.png" },
-    { id: 1, name: "Bronze", imgURL: "../../assets/ranks-images/Bronze.png" },
-    { id: 2, name: "Silver", imgURL: "../../assets/ranks-images/Silver.png" },
-    { id: 3, name: "Gold", imgURL: "../../assets/ranks-images/Gold.png" },
-    {
-      id: 4,
-      name: "Platinum",
-      imgURL: "../../assets/ranks-images/Platinum.png",
-    },
-    { id: 5, name: "Diamond", imgURL: "../../assets/ranks-images/Diamond.png" },
-    { id: 6, name: "Master", imgURL: "../../assets/ranks-images/Master.png" },
-    {
-      id: 7,
-      name: "GrandMaster",
-      imgURL: "../../assets/ranks-images/Grandmaster.png",
-    },
-    {
-      id: 8,
-      name: "Challenger",
-      imgURL: "../../assets/ranks-images/Challenger.png",
-    },
-  ];
-  currentRank = this.leagues[3];
+  leagues = leagues;
+  currentRank = this.leagues[4];
   desiredRank = this.leagues[this.currentRank.id + 1];
 
-  servers: any[] = [
-    { id: 0, name: "EU West" },
-    { id: 1, name: "EU Nordic and East" },
-    { id: 2, name: "NA" },
-    { id: 3, name: "LAN" },
-    { id: 4, name: "OCE" },
-    { id: 5, name: "SEA" },
-    { id: 6, name: "TR" },
-    { id: 7, name: "Russia" },
-    { id: 8, name: "Brazil" },
-  ];
+  servers = servers;
   server = this.servers[0];
 
-  queues: any[] = [
-    { id: 0, name: "Solo Queue" },
-    { id: 1, name: "Flex Queue" },
-    { id: 2, name: "3v3 Queue" },
-  ];
+  queues = queues;
   queue = this.queues[0];
 
-  summoners: any[] = [
-    {
-      id: 0,
-      name: "Any",
-      imgURL: "../../assets/role_positions-images/fill.png",
-    },
-    {
-      id: 1,
-      name: "Flash",
-      imgURL: "../../assets/summoner_spells-images/Flash.png",
-    },
-    {
-      id: 2,
-      name: "Ignite",
-      imgURL: "../../assets/summoner_spells-images/Ignite.png",
-    },
-    {
-      id: 3,
-      name: "Heal",
-      imgURL: "../../assets/summoner_spells-images/Heal.png",
-    },
-    {
-      id: 4,
-      name: "Smite",
-      imgURL: "../../assets/summoner_spells-images/Smite.png",
-    },
-    {
-      id: 5,
-      name: "Teleport",
-      imgURL: "../../assets/summoner_spells-images/Teleport.png",
-    },
-    {
-      id: 6,
-      name: "Exhaust",
-      imgURL: "../../assets/summoner_spells-images/Exhaust.png",
-    },
-    {
-      id: 7,
-      name: "Cleanse",
-      imgURL: "../../assets/summoner_spells-images/Cleanse.png",
-    },
-    {
-      id: 8,
-      name: "Barrier",
-      imgURL: "../../assets/summoner_spells-images/Barrier.png",
-    },
-    {
-      id: 9,
-      name: "Ghost",
-      imgURL: "../../assets/summoner_spells-images/Ghost.png",
-    },
-  ];
+  summoners = summoners;
   summonerKeyD = this.summoners[0];
   summonerKeyF = this.summoners[0];
 
-  roles: any[] = [
-    {
-      id: 0,
-      name: "Any",
-      imgURL: "../../assets/role_positions-images/fill.png",
-    },
-    {
-      id: 1,
-      name: "Top",
-      imgURL: "../../assets/role_positions-images/top.png",
-    },
-    {
-      id: 2,
-      name: "Jungle",
-      imgURL: "../../assets/role_positions-images/jungle.png",
-    },
-    {
-      id: 3,
-      name: "Middle",
-      imgURL: "../../assets/role_positions-images/middle.png",
-    },
-    {
-      id: 4,
-      name: "Bottom",
-      imgURL: "../../assets/role_positions-images/bottom.png",
-    },
-    {
-      id: 5,
-      name: "Support",
-      imgURL: "../../assets/role_positions-images/support.png",
-    },
-  ];
-
+  roles = roles;
   primaryRole = this.roles[0];
   secondaryRole = this.roles[0];
 
-  divisions: any[] = [
-    { id: 0, name: "IV" },
-    { id: 1, name: "III" },
-    { id: 2, name: "II" },
-    { id: 3, name: "I" },
-  ];
-
+  divisions = divisions;
   currentDivision = this.divisions[0];
   desiredDivision = this.divisions[this.currentDivision.id + 1];
 
-  leaguePoints: any[] = [
-    { id: 0, name: "0-29LP" },
-    { id: 1, name: "30-59LP" },
-    { id: 2, name: "60-100LP" },
-  ];
+  leaguePoints = leaguePoints;
   currentLP = this.leaguePoints[0];
 
   constructor() {}
   selectPrimaryRole(id: number) {
+    if (id === this.secondaryRole.id) {
+      this.secondaryRole = this.roles[0];
+    }
     this.primaryRole = this.roles[id];
   }
   selectSecondaryRole(id: number) {
+    if (id === this.primaryRole.id) {
+      this.primaryRole = this.roles[0];
+    }
     this.secondaryRole = this.roles[id];
   }
   clickOutsidePrimaryRole($event) {
@@ -242,9 +126,15 @@ export class OrderMenuV2Component implements OnInit {
     this.queue = this.queues[id];
   }
   selectSummonerKeyD(id: number) {
+    if (id === this.summonerKeyF.id) {
+      this.summonerKeyF = this.summoners[0];
+    }
     this.summonerKeyD = this.summoners[id];
   }
   selectSummonerKeyF(id: number) {
+    if (id === this.summonerKeyD.id) {
+      this.summonerKeyD = this.summoners[0];
+    }
     this.summonerKeyF = this.summoners[id];
   }
   toggleSummonerKeyDMenu() {
---
+++ src/app/shared/variables.ts
@@ -0,0 +1,144 @@
+export const leagues: any[] = [
+  { id: 0, name: "Iron", imgURL: "../../../assets/ranks-images/Iron.png" },
+  { id: 1, name: "Bronze", imgURL: "../../../assets/ranks-images/Bronze.png" },
+  { id: 2, name: "Silver", imgURL: "../../../assets/ranks-images/Silver.png" },
+  { id: 3, name: "Gold", imgURL: "../../../assets/ranks-images/Gold.png" },
+  {
+    id: 4,
+    name: "Platinum",
+    imgURL: "../../../assets/ranks-images/Platinum.png",
+  },
+  {
+    id: 5,
+    name: "Diamond",
+    imgURL: "../../../assets/ranks-images/Diamond.png",
+  },
+  { id: 6, name: "Master", imgURL: "../../../assets/ranks-images/Master.png" },
+  {
+    id: 7,
+    name: "GrandMaster",
+    imgURL: "../../../assets/ranks-images/Grandmaster.png",
+  },
+  {
+    id: 8,
+    name: "Challenger",
+    imgURL: "../../../assets/ranks-images/Challenger.png",
+  },
+];
+
+export const servers: any[] = [
+  { id: 0, name: "EU West" },
+  { id: 1, name: "EU Nordic and East" },
+  { id: 2, name: "NA" },
+  { id: 3, name: "LAN" },
+  { id: 4, name: "OCE" },
+  { id: 5, name: "SEA" },
+  { id: 6, name: "TR" },
+  { id: 7, name: "Russia" },
+  { id: 8, name: "Brazil" },
+];
+
+export const queues: any[] = [
+  { id: 0, name: "Solo Queue" },
+  { id: 1, name: "Flex Queue" },
+  { id: 2, name: "3v3 Queue" },
+];
+
+export const summoners: any[] = [
+  {
+    id: 0,
+    name: "Any",
+    imgURL: "../../../assets/role_positions-images/fill.png",
+  },
+  {
+    id: 1,
+    name: "Flash",
+    imgURL: "../../../assets/summoner_spells-images/Flash.png",
+  },
+  {
+    id: 2,
+    name: "Ignite",
+    imgURL: "../../../assets/summoner_spells-images/Ignite.png",
+  },
+  {
+    id: 3,
+    name: "Heal",
+    imgURL: "../../../assets/summoner_spells-images/Heal.png",
+  },
+  {
+    id: 4,
+    name: "Smite",
+    imgURL: "../../../assets/summoner_spells-images/Smite.png",
+  },
+  {
+    id: 5,
+    name: "Teleport",
+    imgURL: "../../../assets/summoner_spells-images/Teleport.png",
+  },
+  {
+    id: 6,
+    name: "Exhaust",
+    imgURL: "../../../assets/summoner_spells-images/Exhaust.png",
+  },
+  {
+    id: 7,
+    name: "Cleanse",
+    imgURL: "../../../assets/summoner_spells-images/Cleanse.png",
+  },
+  {
+    id: 8,
+    name: "Barrier",
+    imgURL: "../../../assets/summoner_spells-images/Barrier.png",
+  },
+  {
+    id: 9,
+    name: "Ghost",
+    imgURL: "../../../assets/summoner_spells-images/Ghost.png",
+  },
+];
+
+export const roles: any[] = [
+  {
+    id: 0,
+    name: "Any",
+    imgURL: "../../assets/role_positions-images/fill.png",
+  },
+  {
+    id: 1,
+    name: "Top",
+    imgURL: "../../assets/role_positions-images/top.png",
+  },
+  {
+    id: 2,
+    name: "Jungle",
+    imgURL: "../../assets/role_positions-images/jungle.png",
+  },
+  {
+    id: 3,
+    name: "Middle",
+    imgURL: "../../assets/role_positions-images/middle.png",
+  },
+  {
+    id: 4,
+    name: "Bottom",
+    imgURL: "../../assets/role_positions-images/bottom.png",
+  },
+  {
+    id: 5,
+    name: "Support",
+    imgURL: "../../assets/role_positions-images/support.png",
+  },
+];
+
+export const divisions: any[] = [
+  { id: 0, name: "IV" },
+  { id: 1, name: "III" },
+  { id: 2, name: "II" },
+  { id: 3, name: "I" },
+];
+
+export const leaguePoints: any[] = [
+  { id: 0, name: "0-29LP" },
+  { id: 1, name: "30-59LP" },
+  { id: 2, name: "60-100LP" },
+];
