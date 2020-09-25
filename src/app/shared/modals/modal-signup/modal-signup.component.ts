cc8453392207087eafba0cdc52a60f5e90d55f5a
Viktor Ivanov <sessgreem@gmail.com>
2020-09-25 14:34:18 +0300

    Added signup-form module component

--- src/app/shared/modals/modal-signup/modal-signup.component.ts
+++ src/app/shared/modals/modal-signup/modal-signup.component.ts
@@ -1,15 +1,38 @@
-import { Component, OnInit } from '@angular/core';
+import { Validators, FormGroup, FormBuilder } from "@angular/forms";
+import { AuthService } from "src/app/core/services/auth.service";
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-modal-signup',
-  templateUrl: './modal-signup.component.html',
-  styleUrls: ['./modal-signup.component.scss']
+  selector: "app-modal-signup",
+  templateUrl: "./modal-signup.component.html",
+  styleUrls: ["./modal-signup.component.scss"],
 })
 export class ModalSignupComponent implements OnInit {
+  signUpForm: FormGroup;
 
-  constructor() { }
+  constructor(private fb: FormBuilder, private auth: AuthService) {}
 
   ngOnInit(): void {
+    this.signUpForm = this.fb.group({
+      email: ["", [Validators.required, Validators.email]],
+      password: ["", [Validators.required, Validators.minLength(6)]],
+      repeatPassword: ["", [Validators.required, Validators.minLength(6)]],
+    });
   }
 
+  get email() {
+    return this.signUpForm.get("email");
+  }
+  get password() {
+    return this.signUpForm.get("password");
+  }
+  get repeatPassword() {
+    return this.signUpForm.get("repeatPassword");
+  }
+
+  emailSignUp() {
+    if (this.password.value === this.repeatPassword.value) {
+      return this.auth.emailSignUp(this.email.value, this.password.value);
+    }
+  }
 }
